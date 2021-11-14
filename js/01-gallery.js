3805437fc041b07ff7dd4230c8cde882ba26865f
Dmytro Kalistratov <wobupeld@gmail.com>
2021-11-14 18:29:54 +0200

    First part of Homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+let galleryMarkup = '';
+
+galleryItems.forEach((galleryItem) => {
+    
+    const itemMarkup = `<div class="gallery__item">
+  <a class="gallery__link" href=${galleryItem.original}>
+    <img
+      class="gallery__image"
+      src=${galleryItem.preview}
+      data-source=${galleryItem.original}
+      alt=${galleryItem.description}
+    />
+  </a>
+</div>`
+
+    galleryMarkup += itemMarkup;
+});
+
+const galleryElem = document.querySelector('.gallery');
+
+galleryElem.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function zoomImage(event) {
+  event.preventDefault();
+
+  const currentImage = event.target.getAttribute("data-source");
+
+  const modalImage = basicLightbox.create(`<img src=${currentImage} width="1200">`)
+  modalImage.show();
+
+  document.addEventListener('keydown', (event) => { if(event.code === "Escape") {  modalImage.close() }});
+};
+
+
+galleryElem.addEventListener('click', zoomImage);
+
 console.log(galleryItems);
